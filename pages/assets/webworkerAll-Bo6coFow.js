137fbe23498edb080093b5fc0b7e523d1f9ec056
Charlieee1 <77288859+Charlieee1@users.noreply.github.com>
2025-02-27 10:37:38 -0500

    trying to get github pages working

---
+++ pages/assets/webworkerAll-Bo6coFow.js
@@ -0,0 +1,17 @@
+import { __tla as __tla_0 } from "./init-DNQ56oVU.js";
+import { __tla as __tla_1 } from "./index-BQMWuzfD.js";
+Promise.all([
+  (() => {
+    try {
+      return __tla_0;
+    } catch {
+    }
+  })(),
+  (() => {
+    try {
+      return __tla_1;
+    } catch {
+    }
+  })()
+]).then(async () => {
+});
